6ddc2a26df17ee5d0c65b35dcc418fd675564c53
Lucas <l.freittas@outlook.com>
2023-01-09 17:40:31 -0300

    API first files setup

--- src/server.js
+++ src/server.js
@@ -1,14 +1,27 @@
-const { response } = require('express');
-const express = require('express');
+require('express-async-errors');
+const AppError = require('./utils/AppError');
+
+const express = require ('express');
+const routes = require('./routes');
 
 const app = express();
+app.use(express.json());
+app.use(routes);
 
-app.get('/message', (request, response) => {
-    const { id, user } = request.query;
+app.use((error, request, response, next) =>{
+    if(error instanceof AppError){
+        return response.status(error.statusCode).json({
+            status: "error",
+            message: error.message
+        });
+    }
+    console.error(error);
 
-    response.send(`Seu Id é: ${id} e seu nome é ${user}`);
-})
+    return response.status(500).json({
+        status: "error",
+        message: "Internal Server Error"
+    });
+});
 
 const PORT = 3333;
-
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
