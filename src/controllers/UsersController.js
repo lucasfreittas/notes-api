6847933800d5325ef73176585898da9077788420
Lucas <l.freittas@outlook.com>
2023-01-10 19:41:26 -0300

    study

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -2,15 +2,14 @@ const AppError = require('../utils/AppError');
 
 class UsersController{
     create(request, response){
-        const { name, email, password } = request.body;
+        const {name, email, password} = request.body;
 
         if(!name){
-            throw new AppError('Nome é obrigatório');
+            throw new AppError('O nome é obrigatório');
         }
 
-        response.json({name, email, password})
-
-    };
-};
+        response.json({name,email,password});
+    }
+}
 
 module.exports = UsersController;
--- src/server.js
+++ src/server.js
@@ -1,29 +1,30 @@
-require('express-async-errors');
 const AppError = require('./utils/AppError');
+const express = require('express');
 const routes = require('./routes');
-const PORT = 3333;
-const express = require('express')
 const database = require('./database/sqlite/migrations');
-
+const PORT = 3333;
 
 const app = express();
 app.use(express.json());
 app.use(routes);
-
 database();
+
 app.use((error, request, response, next) => {
     if(error instanceof AppError){
         return response.status(error.statusCode).json({
             status: "error",
             message: error.message
-        })
+        });
     }
     console.log(error);
 
     return response.status(500).json({
-        stauts: "error",
+        status: "error",
         message: "Internal Server Error"
     });
 });
 
 app.listen(PORT, () => console.log(`Server is running on PORT: ${PORT}`));
+
+
+
