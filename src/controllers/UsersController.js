c436e7d6fd129f8000fd9c54486d32792ef1909a
Lucas <l.freittas@outlook.com>
2023-01-11 15:23:40 -0300

    crypto passwords

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,14 +1,33 @@
 const AppError = require('../utils/AppError');
+const sqlConnection = require('../database/sqlite')
+const { hash } = require('bcryptjs');
 
 class UsersController{
-    create(request, response){
+    async create(request, response){
         const {name, email, password} = request.body;
 
-        if(!name){
-            throw new AppError('O nome é obrigatório')
-        };
+        //Conexão com o banco de dados
+        const database = await sqlConnection();
 
-        response.json({name, email, password})
+         //Checkando se o email já existe
+         const checkEmailExisists = await database.get('SELECT * FROM users WHERE email = (?)', [email])
+         if(checkEmailExisists){
+             throw new AppError('Este e-mail já está em uso')
+         };
+
+        //Criptografando a senha
+        const hashedPassword = await hash(password, 8);
+
+        //Criação de usuário no banco
+        await database.run(
+            "INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
+            [name, email, hashedPassword]
+        );
+        return response.status(201).json();
+        
+       
+
+        
     };
 };
 
